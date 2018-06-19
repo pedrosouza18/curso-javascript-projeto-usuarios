35f663b446d60cd174e1328c6142901d56d8c26b
Pedro Souza <souzapedro70@gmail.com>
2018-06-19 16:14:01 -0300

    Novos ajustes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,17 +1,20 @@
 class UserController {
 
-    constructor(formId) {
+    constructor(formId, tableId) {
 
         var $ = document.querySelector.bind(document);
 
         this._form = $(formId);
+        this._table = $(tableId);
+
+        this.onSubmit();
     }
 
     getValues() {
 
         let user = {};
 
-        this._form.elements.forEach(function(element, idx) {
+        Array.from(this._form.elements).forEach(function(element, idx) {
             if(element.name == 'gender') {
                 if(element.checked) user[element.name] = element.value;
             } else {
@@ -23,20 +26,20 @@ class UserController {
     }
 
     onSubmit() {
-        this._form.addEventListener('submit', (ev) => {
-            ev.preventDefault();
-            this.getValues();
+        this._form.addEventListener("submit", event => {
+            event.preventDefault();
+            this.addLine(this.getValues());
         });
     }
 
-    addLine(dataUser, tableId) {
-        $(tableId).innerHTML = `
+    addLine(dataUser) {
+        this._table.innerHTML = `
         <tr>
             <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${dataUser.admin}</td>
-            <td>${formatDate(dataUser.birth)}</td>
+            <td>${dataUser.birth}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
